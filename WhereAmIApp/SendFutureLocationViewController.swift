5da9777065d5c10b593094b8497271e9a010502f
Uğur <ugur@Ugurs-MacBook-Pro.local>
2015-05-22 02:34:04 +0300

    Updated sending future location and date info

--- WhereAmIApp/SendFutureLocationViewController.swift
+++ WhereAmIApp/SendFutureLocationViewController.swift
@@ -8,25 +8,34 @@
 
 import UIKit
 
-class SendFutureLocationViewController: UIViewController {
+class SendFutureLocationViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var btnSendImage: UIView!
     @IBOutlet weak var messageView: UIView!
     @IBOutlet weak var lblMessage: UILabel!
     @IBOutlet weak var lblLocation: UILabel!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var toolBar: UIToolbar!
+    @IBOutlet weak var txtDate: UITextField!
+    
+    var isPlaceSelected = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        var fbBtn = FBSDKMessengerShareButton.circularButtonWithStyle(.Blue)
+        
+        var buttonWidth = CGFloat(80)
+        var fbBtn = FBSDKMessengerShareButton.circularButtonWithStyle(.Blue,  width:buttonWidth)
         fbBtn.addTarget(self, action: "_shareButtonPressed:" , forControlEvents: .TouchUpInside)
         //        fbBtn.sizeThatFits(CGSize(width: 200, height: 45))
         //setting for fbBtn if needed
         self.btnSendImage.addSubview(fbBtn)
-
+        
+        self.txtDate.delegate = self
+        datePicker.backgroundColor = UIColor(red: 42, green: 140, blue: 162, alpha: 1)
+        datePicker.addTarget(self, action: Selector("datePickerChanged:"), forControlEvents: UIControlEvents.ValueChanged)
         // Do any additional setup after loading the view.
     }
-
+ 
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(true)
         lblMessage.hidden = true
@@ -37,15 +46,18 @@ class SendFutureLocationViewController: UIViewController {
             lblMessage.text = locationText
             let appDomain = NSBundle.mainBundle().bundleIdentifier
             NSUserDefaults.standardUserDefaults().removePersistentDomainForName(appDomain!)
+            
+            isPlaceSelected = true
         }
     }
     
     @IBAction func _shareButtonPressed(sender: AnyObject) {
+        if isPlaceSelected && txtDate.text != ""{
         let result = FBSDKMessengerSharer.messengerPlatformCapabilities().rawValue & FBSDKMessengerPlatformCapability.Image.rawValue
         if result != 0 {
             lblMessage.hidden = false
-            lblMessage.text = lblLocation.text
-            UIGraphicsBeginImageContext(CGSize(width: 360, height: 40))
+            lblMessage.text = "Hey! I will be at " + lblLocation.text! + " at " + self.txtDate.text
+            UIGraphicsBeginImageContext(CGSize(width: 360, height: 80))
             messageView.layer.renderInContext(UIGraphicsGetCurrentContext())
             let sharingImage = UIGraphicsGetImageFromCurrentImageContext()
             UIGraphicsEndImageContext()
@@ -60,14 +72,77 @@ class SendFutureLocationViewController: UIViewController {
             // not installed then open link. Note simulator doesn't open iTunes store.
             UIApplication.sharedApplication().openURL(NSURL(string: "itms://itunes.apple.com/us/app/facebook-messenger/id454638411?mt=8")!)
         }
+        }
+        else{
+            let alertController = UIAlertController(title: "WhereAmIApp", message: "Please select a date and location", preferredStyle: .Alert)
+            
+            let OKAction = UIAlertAction(title: "OK", style: .Default) { (action) in
+                
+            }
+            alertController.addAction(OKAction)
+            self.presentViewController(alertController, animated: true) {
+                // ...
+            }
+        }
     }
 
+    func datePickerChanged(datePicker:UIDatePicker) {
+        var dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "dd.MM.yyyy HH:mm"
+        let strDate = dateFormatter.stringFromDate(datePicker.date)
+        txtDate.text = strDate
+    }
     
+    func textFieldShouldBeginEditing(textField: UITextField) -> Bool{
+        
+        if textField === txtDate {
+            txtDate.resignFirstResponder()
+            datePicker.hidden = false
+            toolBar.hidden = false
+            btnSendImage.hidden = true
+            return false
+        }
+        return   true
+    }
+    
+    @IBAction func hideDatePicker(sender: AnyObject) {
+        datePicker.hidden = true
+        toolBar.hidden = true
+        btnSendImage.hidden = false
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
+        println("User Logged In")
+        
+        if ((error) != nil)
+        {
+            // Process error
+        }
+        else if result.isCancelled {
+            // Handle cancellations
+        }
+        else {
+            // If you ask for multiple permissions at once, you
+            // should check if specific permissions missing
+            if result.grantedPermissions.contains("email")
+            {
+                self.performSegueWithIdentifier("send_loc", sender: self)
+            }
+            
+            //  self.returnUserData()
+        }
+        
+    }
+    
+    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
+        println("User Logged Out")
+        self.navigationController?.popViewControllerAnimated(true)
+    }
+
 
     /*
     // MARK: - Navigation
